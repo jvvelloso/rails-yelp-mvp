78a132ff343424e93514e0bf736f99ed90194958
Joao Velloso <joao.velloso@rfb.gov.br>
2020-11-27 17:56:09 -0300

    rails-yelp-mvp models ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: [0..5],
-                                                  message: '%{value} is not a valid rating' }
+  validates :rating, presence: true, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
